c0d5ed4cd9278052dc95c6ef4e311154d59d5d6f
Tim Roberts <imtimrob@gmail.com>
2022-12-18 15:01:42 -0600

    Added comments for toggling hidden files

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,4 +3,6 @@ vim.g.netrw_banner = 0 -- Hide banner
 vim.g.netrw_preview = 1
 vim.g.netrw_altv = 1 -- Open with right splitting
 vim.g.netrw_liststyle = 3 -- Tree-style view
+--Hide 'dotfiles' Toggle hidden with 'gh'
 vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignorek
+
