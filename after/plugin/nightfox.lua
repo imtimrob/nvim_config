1f9fbf38f949d0fb8a33e8c5d51c1fb11375fd77
Tim Roberts <imtimrob@gmail.com>
2022-12-28 18:50:16 -0600

    Altered Nightfox visual selection color

--- after/plugin/nightfox.lua
+++ after/plugin/nightfox.lua
@@ -1,25 +1,30 @@
 require("nightfox").setup({
   palettes = {
-    -- Custom duskfox with black background
+    -- Custom carbonfox with black background
     carbonfox = {
-    --  bg1 = "#000000", -- Black background
+      bg1 = "#000000", -- Black background
      -- bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
      -- bg3 = "#121820", -- 55% darkened from stock
-      sel0 = "#800080", -- 55% darkened from stock
-      sel1 = "#ad00ad",
+      sel0 = "#791785", -- 55% darkened from stock
+      sel1 = "#791785",
     },
   },
-  specs = {
-    all = {
-      inactive = "bg0", -- Default value for other styles
-    },
-    duskfox = {
-      inactive = "#090909", -- Slightly lighter then black background
-    },
-  },
-  groups = {
-    all = {
-      NormalNC = { fg = "fg1", bg = "inactive" }, -- Non-current windows
-    },
-  },
+--  specs = {
+--    all = {
+--      inactive = "bg0", -- Default value for other styles
+--    },
+--    duskfox = {
+--      inactive = "#090909", -- Slightly lighter then black background
+--    },
+--  },
+--  groups = {
+--    all = {
+--      NormalNC = { fg = "fg1", bg = "inactive" }, -- Non-current windows
+--    },
+--  },
 })
+
+--local override = require('nightfox').override
+--override.palettes({ carbonfox = { sel1 = "565656", },
+--})
+--
