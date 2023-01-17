351ff43bf8f05ad23d44b281041035c978fc15da
Tim Roberts <imtimrob@gmail.com>
2023-01-17 10:43:55 -0600

    Updated lsp.lua for completion

--- lua/timrob/packer.lua
+++ lua/timrob/packer.lua
@@ -8,65 +8,69 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
-    use 'BurntSushi/ripgrep'
+  use 'BurntSushi/ripgrep'
 
-    use({
-        'EdenEast/nightfox.nvim',
-        as = 'carbonfox',
-        config = function()
-            vim.cmd('colorscheme carbonfox')
-        end
-    })
+  use({
+    'EdenEast/nightfox.nvim',
+    as = 'carbonfox',
+    config = function()
+      vim.cmd('colorscheme carbonfox')
+    end
+  })
 
-    use {
-        'nvim-treesitter/nvim-treesitter', 
-        run = ':TSUpdate',
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = 'all',
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                }
-            }
-        end,
-    }
-
-    use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('mattn/emmet-vim')
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
+  use {
+    'nvim-treesitter/nvim-treesitter', 
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = 'all',
+        auto_install = true,
+        highlight = {
+          enable = true,
         }
+      }
+    end,
+  }
+
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use('mattn/emmet-vim')
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
     }
+  }
 end)
 
--- lua/timrob/remap.lua
+++ lua/timrob/remap.lua
@@ -52,3 +52,13 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Markdown Preview
+-- normal/insert
+--<Plug>MarkdownPreview
+--<Plug>MarkdownPreviewStop
+--<Plug>MarkdownPreviewToggle
+
+-- example
+vim.keymap.set("n", "<C-s>", "<Plug>MarkdownPreview")
+vim.keymap.set("n", "<M-s>", "<Plug>MarkdownPreviewStop")
+vim.keymap.set("n", "<C-p>", "<Plug>MarkdownPreviewToggle")
