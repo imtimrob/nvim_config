a62abe68f8a7e26d3da80dab97eb048fafa6c4fe
imtimrob <imtimrob@gmail.com>
2023-04-30 13:39:50 -0500

    edit packer.lua

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,6 +1,6 @@
 -- Better Netrw
 vim.g.netrw_banner = 0 -- Hide banner
---vim.g.netrw_browse_split = 2
+vim.g.netrw_browse_split = 4
 vim.g.netrw_preview = 1
 --vim.g.netrw_altv = 1 -- Open with right splitting
 vim.g.netrw_liststyle = 0 -- Tree-style view
@@ -8,4 +8,4 @@ vim.g.netrw_liststyle = 0 -- Tree-style view
 --Hide 'dotfiles' Toggle hidden with 'gh'
 vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
 
-vim.g.netrw_winsize = 20
+vim.g.netrw_winsize = 30
