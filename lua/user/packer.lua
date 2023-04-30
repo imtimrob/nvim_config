2665eba9921ca273280545b6a2c7f961ab536961
imtimrob <imtimrob@gmail.com>
2023-04-30 10:45:10 -0500

    refactoring packer

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,87 +1,116 @@
 -- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
-require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
+]])
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                            , branch = '0.1.x',
-    requires =  {'nvim-lua/plenary.nvim'}
-  }
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-
-
-  use({
-    'EdenEast/nightfox.nvim',
-    as = 'carbonfox',
-    config = function()
-      vim.cmd('colorscheme carbonfox')
-    end
-  })
-
-  use {
-    'nvim-treesitter/nvim-treesitter', 
-    run = ':TSUpdate',
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = 'all',
-        auto_install = true,
-        highlight = {
-          enable = true,
-        }
-      }
-    end,
-  }
-
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('mattn/emmet-vim')
-  use 'BurntSushi/ripgrep'
-
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+return packer.startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use 'nvim-lua/popup.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-      -- Useful status updates for LSP
-      {'j-hui/fidget.nvim'},
 
-      -- Additional lua configuration, makes nvim stuff amazing
-      {'folke/neodev.nvim'},
+	use({
+		'EdenEast/nightfox.nvim',
+		as = 'carbonfox',
+		config = function()
+			vim.cmd('colorscheme carbonfox')
+		end
+	})
 
-    }
-  }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
+		config = function()
+			require 'nvim-treesitter.configs'.setup {
+				ensure_installed = 'all',
+				auto_install = true,
+				highlight = {
+					enable = true,
+				}
+			}
+		end,
+	}
+
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use('mattn/emmet-vim')
+	use 'BurntSushi/ripgrep'
+	use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
+	use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+	use 'numToStr/Comment.nvim'               -- "gc" to comment visual regions/lines
+	use 'tpope/vim-sleuth'                    -- Detect tabstop and shiftwidth automatically
+
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' },
+			{ 'williamboman/mason.nvim' },
+			{ 'williamboman/mason-lspconfig.nvim' },
+
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' },
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'saadparwaiz1/cmp_luasnip' },
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/cmp-nvim-lua' },
+
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' },
+			{ 'rafamadriz/friendly-snippets' },
+			-- Useful status updates for LSP
+			{ 'j-hui/fidget.nvim' },
+
+			-- Additional lua configuration, makes nvim stuff amazing
+			{ 'folke/neodev.nvim' },
+
+		}
+	}
 end)
-
